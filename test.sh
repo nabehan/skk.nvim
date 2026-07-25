2ac601fa0d3d3c459a811fc2c07d84ea8f7bf95b
nabehan <y.nabekura@gmail.com>
2026-07-25 10:17:57 +0900

    fix: tests

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-#/usr/bin/sh
+#!/usr/bin/sh
 
 PLENARY_DIR=~/.local/share/nvim/lazy/plenary.nvim nvim --headless --noplugin -u tests/minimal_init.lua -c "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}"
